b9d55a6e2bbc59d3ab81e5747f86ca778195f950
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-08-18 13:59:05 +0200

    Added support for getting basic test metadatas.

--- src/core.lua
+++ src/core.lua
@@ -1,5 +1,29 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 local core = {
 }
+
+function core.isVerboseOptionUsed(options)
+    return options["v"] or options["verbose"]
+end
+
+function core.isDryRunOptionUsed(options)
+    return options["d"] or options["dry-run"]
+end
 
 function core.isHelpOptionUsed(options)
     return options["h"] or options["help"]
@@ -9,30 +33,67 @@ function core.isVersionOptionUsed(options)
     return options["V"] or options["version"]
 end
 
-function core.isDryRunOptionUsed(options)
-    return options["d"] or options["dry-run"]
+function core.isLicenseOptionUsed(options)
+    return options["L"] or options["license"]
 end
 
-function core.showHelp()
-    print([[DocUnit version 0.0.1
-    Usage:
-        -d, --dry-run
-         Do not run any tests, just print their names to standard output.
-
-        -h, --help
-         Print usage information and exit.
-
-        -V, --version
-         Print version information and exit.
-    ]])
+function core.isVerborsOptionUsed(options)
+    return options["v"] or options["verbose"]
 end
 
 function core.showVersion()
-    print("DocUnit version 0.0.1")
+    print("Emender version 0.0.1")
 end
 
-function core.performDryRun()
-    -- TODO
+function core.updateTestName(filename)
+    if filename:endsWith(".lua") then
+        local extensionIndex = filename:find(".lua", 1, true)
+        if extensionIndex then
+            return filename:substring(1, extensionIndex - 1)
+        else
+            return nil
+        end
+    else
+        return nil
+    end
+end
+
+function core.printDetailedTestInfo(test)
+    local metadata = test["metadata"]
+    if not metadata then
+        print("Test " .. test .. " does not contain metadata, needs to be fixed!")
+    end
+    local description = metadata["description"]
+    print(description)
+    local author = metadata["author"]
+    print(author)
+end
+
+function core.printTestInfo(scriptDirectory, filename, verboseOperation)
+    local testName = core.updateTestName(filename)
+    if testName then
+        if _ENV[testName] then
+            print("Warning: test " .. testName .. " might overwrite core functionality!")
+            print("You'd need to rename it")
+        end
+        dofile(scriptDirectory .. "tests/" .. filename)
+        -- if test is properly loaded
+        local test = _ENV[testName]
+        if test then
+            print("Test: " .. testName)
+            if verboseOperation then
+                core.printDetailedTestInfo(test)
+            end
+        end
+    end
+end
+
+function core.performDryRun(verboseOperation)
+    local scriptDirectory = getScriptDirectory()
+    local command = 'ls -1 '.. scriptDirectory .. "tests/*.lua"..'| xargs -n 1 basename'
+    for filename in io.popen(command):lines() do
+        core.printTestInfo(scriptDirectory, filename, verboseOperation)
+    end
 end
 
 return core
