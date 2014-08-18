bfe5dc9a2a7f813177d8cf26668231473190b3a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-08-18 13:26:31 +0200

    Added Test2.lua, renamed Test1.lua, added license.

---
+++ tests/Test2.lua
@@ -0,0 +1,52 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+Test2 = {
+    -- required field
+    metadata = {
+        description = "This is the second test",
+        author = "Pavel Tisnovsky",
+        email = "ptisnovs@redhat.com",
+        tags = {"XMLcheck", "BasicTest", "SmokeTest"},
+    },
+    -- any other values are arbitrary
+    val = 42}
+
+function Test2:testA()
+    print("Test2:testA()")
+end
+
+function Test2:testB()
+    print("Test2:testB()")
+end
+
+function Test2:xyzzy()
+    print(self.val)
+end
+
+function Test2:foo()
+    print("This method should not be called from the tool core!")
+end
+
+function Test2:bar()
+    self:foo()
+end
+
+function Test2:testLocalValue()
+    print("Test2:testLocalValue()")
+    print(self.val)
+end
+
