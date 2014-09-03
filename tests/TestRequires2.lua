f763d378aca7128fdbdbf4f8aee2524582633785
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-03 14:46:10 +0200

    Added tests that check "requires" attribute.

---
+++ tests/TestRequires2.lua
@@ -0,0 +1,33 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestRequires2 = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-08-25",
+        tags = {"XMLcheck", "BasicTest", "SmokeTest"},
+    },
+    -- check if the tool work properly with empty table
+    requires = {}
+}
+
+function TestRequires2.testA()
+    print("\tTestRequires2.testA()")
+end
+
