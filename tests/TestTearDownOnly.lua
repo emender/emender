771d7a7741b45959fa64cec0ea3a1e9212acfb13
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-08 15:27:10 +0200

    More tests for setUp() and tearDown() functions.

---
+++ tests/TestTearDownOnly.lua
@@ -0,0 +1,31 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestTearDownOnly = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly - test contains tearDown() function only.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-09-08",
+        tags = {"BasicTest"},
+    }
+}
+
+function TestTearDownOnly.tearDown()
+    print("\ttearDown() function called")
+end
+
