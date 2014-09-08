95441a4508e330ebfecdc4c47d447fe2e061cab7
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-08 15:15:54 +0200

    Added test that checks the "special" setUp() function.

---
+++ tests/TestSetUp.lua
@@ -0,0 +1,39 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestSetUp = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-08-25",
+        tags = {"BasicTest"},
+    }
+}
+
+function TestSetUp.testA()
+    print("\tTestSetUp.testA()")
+end
+
+function TestSetUp.testB()
+    print("\tTestSetUp.testB()")
+end
+
+function TestSetUp.setUp()
+    print("\tsetUp() function called")
+end
+
