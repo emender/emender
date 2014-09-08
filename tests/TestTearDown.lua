28e5571d67630df409bf5b4a450d7e977e37ff42
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-08 15:17:35 +0200

    Added test that checks the "special" tearDown() function.

---
+++ tests/TestTearDown.lua
@@ -0,0 +1,39 @@
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestTearDown = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-08-25",
+        tags = {"BasicTest"},
+    }
+}
+
+function TestTearDown.testA()
+    print("\tTestTearDown.testA()")
+end
+
+function TestTearDown.testB()
+    print("\tTestTearDown.testB()")
+end
+
+function TestTearDown.tearDown()
+    print("\tTearDown() function called")
+end
+
