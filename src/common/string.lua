b51b278a6387f5b88e41a52f7de0e23f204247ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-19 16:32:20 +0200

    Added (c).

--- src/common/string.lua
+++ src/common/string.lua
@@ -1,5 +1,6 @@
 --
 -- This file is part of Emender.
+-- Copyright (C) 2014 Pavel Tisnovsky, Jaromir Hradilek
 --
 -- Emender is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
