0216433c522c470cfc83c7d2995fa4c2ef751af1
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-22 12:59:04 +0200

    Updated the test TestAssertionFail to check other improper cases.

--- test/TestAssertionFail.lua
+++ test/TestAssertionFail.lua
@@ -1,5 +1,5 @@
 -- TestAssertionFail.lua - test the behavior of the asserts.fail() function
--- Copyright (C) 2014 Jaromir Hradilek
+-- Copyright (C) 2014 Jaromir Hradilek, Pavel Tisnovsky
 
 -- This file is part of Emender
 
@@ -18,34 +18,84 @@
 TestAssertionFail = {
     metadata = {
         description = "Test the behavior of the asserts.fail() function.",
-        authors = "Jaromir Hradilek",
-        emails = "jhradilek@redhat.com",
-        changed = "2014-09-10",
+        authors = "Jaromir Hradilek, Pavel Tisnovsky",
+        emails = "jhradilek@redhat.com, ptisnovs@redhat.com",
+        changed = "2014-09-22",
         tags = {"BasicTest"}
     }
 }
 
 -- Verify that the function accepts a string as its argument:
-function TestAssertionFail.testNegative1()
+function TestAssertionFail.testNegative01()
     fail("(expected) The fail() function expects a string as its argument.")
 end
 
 -- Verify that the function requires at least one argument:
-function TestAssertionFail.testNegative2()
+function TestAssertionFail.testNegative02()
     fail()
 end
 
 -- Verify that the function does not accept nil as an argument:
-function TestAssertionFail.testNegative3()
+function TestAssertionFail.testNegative03()
     fail(nil)
 end
 
 -- Verify that the function does not accept a boolean as an argument:
-function TestAssertionFail.testNegative4()
+function TestAssertionFail.testNegative04()
     fail(true)
 end
 
--- Verify that the function does not accept a number as an argument:
-function TestAssertionFail.testNegative5()
+-- Verify that the function does not accept a boolean as an argument:
+-- (and "false" sometimes has the same meaning as "nil", so check it)
+function TestAssertionFail.testNegative05()
+    fail(false)
+end
+
+-- Verify that the function does not accept an integer as an argument:
+function TestAssertionFail.testNegative06()
     fail(42)
 end
+
+-- Verify that the function does not accept a real number as an argument:
+function TestAssertionFail.testNegative07()
+    fail(42.1)
+end
+
+-- Verify that the function does not accept a table as an argument:
+function TestAssertionFail.testNegative08()
+    fail({1,2,3})
+end
+
+-- Verify that the function does not accept a standard function as an argument:
+function TestAssertionFail.testNegative09()
+    fail(print)
+end
+
+-- Verify that the function does not accept an user defined function as an argument:
+function TestAssertionFail.testNegative10()
+    local func = function()
+        return 42
+    end
+    fail(func)
+end
+
+-- Verify that the function does not accept a closure as an argument:
+function TestAssertionFail.testNegative11()
+    local closure = function()
+        local i
+        return function()
+            i = i + 1
+            return i
+        end
+    end
+    fail(closure)
+end
+
+-- Verify that the function does not accept a coroutine as an argument:
+function TestAssertionFail.testNegative12()
+    local func = coroutine.create(function ()
+           print(42)
+         end)
+    fail(func)
+end
+
