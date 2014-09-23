76b95205aa9dbe4f793411cbbfee1a27e8bbd737
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-09-23 12:01:46 +0200

    Added more positive tests to check if is_equal works with tables.

--- test/TestAssertionIsEqual.lua
+++ test/TestAssertionIsEqual.lua
@@ -20,25 +20,85 @@ TestAssertionIsEqual = {
         description = "Test the behaviour function asserts.is_equal().",
         authors = "Pavel Tisnovsky",
         emails = "ptisnovs@redhat.com",
-        changed = "2014-08-28",
+        changed = "2014-09-22",
         tags = {"BasicTest"},
     },
 }
 
-function TestAssertionIsEqual.testPositive1()
+function TestAssertionIsEqual.testPositive01()
     is_equal(true, true, "positive equality test:two booleans")
 end
 
-function TestAssertionIsEqual.testPositive2()
+function TestAssertionIsEqual.testPositive02()
     is_equal(false, false, "positive equality test:two booleans")
 end
 
-function TestAssertionIsEqual.testPositive3()
-    is_equal(1, 1, "positive equality test: two integers")
+function TestAssertionIsEqual.testPositive03()
+    is_equal(42, 42, "positive equality test: two integers")
 end
 
-function TestAssertionIsEqual.testPositive4()
+function TestAssertionIsEqual.testPositive04()
+    is_equal(3.14, 3.14, "positive equality test: two floats")
+end
+
+function TestAssertionIsEqual.testPositive05()
+    is_equal("", "", "positive equality test: two empty strings")
+end
+
+function TestAssertionIsEqual.testPositive06()
     is_equal("x", "x", "positive equality test: two strings")
+end
+
+function TestAssertionIsEqual.testPositive07()
+    is_equal({}, {}, "positive equality test: two empty tables")
+end
+
+function TestAssertionIsEqual.testPositive08()
+    is_equal({1,2}, {1,2}, "positive equality test: two tables, items are integers")
+end
+
+function TestAssertionIsEqual.testPositive09()
+    is_equal({1,2,3,4}, {1,2,3,4}, "positive equality test: two tables, items are integers")
+end
+
+function TestAssertionIsEqual.testPositive10()
+    is_equal({"x", "y"}, {"x", "y"}, "positive equality test: two tables, items are strings")
+end
+
+function TestAssertionIsEqual.testPositive11()
+    is_equal({1,{2,3},4}, {1,{2,3},4}, "positive equality test: two nested tables")
+end
+
+function TestAssertionIsEqual.testPositive12()
+    is_equal({1,{2,{3}},4}, {1,{2,{3}},4}, "positive equality test: two deeply nested tables")
+end
+
+function TestAssertionIsEqual.testPositive13()
+    local table1 = {}
+    local table2 = {}
+
+    table1["first"] = 1
+    table1["second"] = 2
+    table1["third"] = 3
+
+    table2["first"] = 1
+    table2["second"] = 2
+    table2["third"] = 3
+    is_equal(table1, table2, "positive equality test: two hash tables")
+end
+
+function TestAssertionIsEqual.testPositive14()
+    local table1 = {}
+    local table2 = {}
+
+    table1["first"] = "!"
+    table1["second"] = "@"
+    table1["third"] = "#"
+
+    table2["first"] = "!"
+    table2["second"] = "@"
+    table2["third"] = "#"
+    is_equal(table1, table2, "positive equality test: two hash tables")
 end
 
 function TestAssertionIsEqual.testNegative1()
