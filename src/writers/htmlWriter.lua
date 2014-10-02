a536a40a68c2e2880546467e8d4bdc7fc297aa7b
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-02 09:51:38 +0200

    Added missing licence text to htmlWriter.lua.

--- src/writers/htmlWriter.lua
+++ src/writers/htmlWriter.lua
@@ -1,3 +1,20 @@
+-- htmlWriter.lua - a writer for the HTML file format
+-- Copyright (C) 2014 Pavel Tisnovsky
+
+-- This file is part of Emender.
+
+-- This program is free software:  you can redistribute it and/or modify it
+-- under the terms of  the  GNU General Public License  as published by the
+-- Free Software Foundation, version 3 of the License.
+--
+-- This program  is  distributed  in the hope  that it will be useful,  but
+-- WITHOUT  ANY WARRANTY;  without  even the implied warranty of MERCHANTA-
+-- BILITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+-- License for more details.
+--
+-- You should have received a copy of the GNU General Public License  along
+-- with this program. If not, see <http://www.gnu.org/licenses/>.
+
 local htmlWriter = {
     testSuiteCounter = 0,
     toggleCounter = 0,
