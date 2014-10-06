c318a778775cf1c2f6e63d914ec7de967c19401c
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-06 12:08:10 +0200

    Updated Test1.lua.

--- test/Test1.lua
+++ test/Test1.lua
@@ -1,3 +1,5 @@
+-- Test1.lua - check for basic Emender functionality
+-- Copyright (C) 2014 Pavel Tisnovsky
 --
 -- This file is part of Emender.
 --
@@ -26,37 +28,53 @@ Test1 = {
     -- any other values are arbitrary
     val = 42}
 
+--
+-- Comment bound to the function Test1.testA().
+--
 function Test1.testA()
-    print("\tTest1.testA()")
+    pass("The function Test1.testA() is called properly.")
 end
 
+--
+-- Comment bound to the function Test1.testB().
+--
 function Test1.testB()
-    print("\tTest1.testB()")
+    pass("The function Test1.testB() is called properly.")
 end
 
 function Test1.xyzzy()
-    print("\t" .. Test1.val)
+    pass("The function Test1.xyzzy() is called from another function.")
 end
 
 function Test1.foo()
-    print("This method should not be called from the tool core!")
+    fail("This method should not be called from the tool core!")
 end
 
 function Test1.bar()
     self:foo()
+    fail("This method should not be called from the tool core!")
 end
 
+-- This is one line comment.
 function Test1.testReadLocalValue()
-    print("\t" .. Test1.val)
+    pass("Local variable bind to the test has the value " .. Test1.val)
 end
 
+--
+-- This
+-- is
+-- multiline
+-- comment.
+--
 function Test1.testCallOtherFunction()
     Test1.xyzzy()
 end
 
 function Test1.testReadWriteLocalValue()
+    is_true(Test1.val == 42, "Old value is properly set to 42")
     print("\tOld value: " .. Test1.val)
     Test1.val = 6502
+    is_true(Test1.val == 6502, "New value is properly set to 42")
     print("\tNew value: " .. Test1.val)
 end
 
