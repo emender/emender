fda2ac4bcf396845f9c3dbf878a70a38114044bc
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-10 21:33:26 +0200

    Added cleanup script.

---
+++ selfchecks/cleanup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Clean up the selfcheck directory.
+#
+# This file is part of Emender.
+#
+# Emender is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# Emender is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+rm *.out
+rm *.diff
+rm errors.txt
+
