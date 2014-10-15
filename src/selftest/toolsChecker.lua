20f7ef8059365b5565c4db1fff976fcd5fe1b3c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-15 12:03:10 +0200

    Added comments, minor fixes.

--- src/selftest/toolsChecker.lua
+++ src/selftest/toolsChecker.lua
@@ -15,29 +15,49 @@
 -- You should have received a copy of the GNU General Public License  along
 -- with this program. If not, see <http://www.gnu.org/licenses/>.
 
+
+
+--
+-- Data structure that holds all exported variables and functions
+--
 local toolsChecker = {
     requiredTools = {"curl", "wget", "xmllint"}
 }
+
+
 
 --
 -- Check if given command exists (and could be started)
 --
 function commandExists(command)
+    -- run the given command
+    -- NOTE: it does not work properly for interactive commands
     local process = io.popen(command .. " 2> /dev/null")
+    -- read exit value (might indicate error) and process return code
     local exitValue, signal, code = process:close()
     -- TODO: is this code always stable?
     return code ~= 127
 end
 
+
+
+--
+-- Print a message when some external tool is about to be checked about its presence.
+--
 function commandCheckMessage(command, result)
     print("Checking existence of command '" ..  command .. "': " .. result)
 end
 
+
+
 --
 -- Perform check if all required commands are installed.
+-- If any of required tools is not intalled, the process fail.
 --
 function toolsChecker.performCheck()
-    for i, command in ipairs(toolsChecker.requiredTools) do
+    -- loop over all required tools
+    for _, command in ipairs(toolsChecker.requiredTools) do
+        -- check for tool existence
         local exists = commandExists(command)
         if not exists then
             commandCheckMessage(command, "error")
@@ -49,5 +69,10 @@ function toolsChecker.performCheck()
     end
 end
 
+
+
+--
+-- Export the module API
+--
 return toolsChecker
 
