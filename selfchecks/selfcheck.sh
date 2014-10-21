f43f19a0f60d21fd2fb5419a6c17ebacf42eb66e
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-21 11:26:31 +0200

    Added two new selfchecks.

--- selfchecks/selfcheck.sh
+++ selfchecks/selfcheck.sh
@@ -65,11 +65,13 @@ function runTestsWithBadOutputFormat {
 
 # Run Emender with some example tests with known output
 function runTestExamples {
-     ../emend ../test/TestEmpty.lua        > test_empty.out
-     ../emend ../test/TestSetUp.lua        > test_setup.out
-     ../emend ../test/TestSetUpOnly.lua    > test_setup_only.out
-     ../emend ../test/TestTearDown.lua     > test_teardown.out
-     ../emend ../test/TestTearDownOnly.lua > test_teardown_only.out
+     ../emend ../test/TestEmpty.lua             > test_empty.out
+     ../emend ../test/TestSetUp.lua             > test_setup.out
+     ../emend ../test/TestSetUpOnly.lua         > test_setup_only.out
+     ../emend ../test/TestSetUpTearDownOnly.lua > test_setup_teardown_only.out
+     ../emend ../test/TestTearDown.lua          > test_teardown.out
+     ../emend ../test/TestTearDownOnly.lua      > test_teardown_only.out
+     ../emend ../test/TestPassFail.lua          > test_pass_fail.out
 }
 
 
