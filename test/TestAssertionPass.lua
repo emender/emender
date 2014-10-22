90596dc8aa3f7c287f3e3efb0d822664edb98c0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-10-22 10:38:33 +0200

    Updated, added comments and some new test cases.

--- test/TestAssertionPass.lua
+++ test/TestAssertionPass.lua
@@ -20,67 +20,157 @@ TestAssertionPass = {
         description = "Test the behavior of the asserts.pass() function.",
         authors = "Jaromir Hradilek, Pavel Tisnovsky",
         emails = "jhradilek@redhat.com, ptisnovs@redhat.com",
-        changed = "2014-09-22",
+        changed = "2014-10-22",
         tags = {"BasicTest"}
     }
 }
 
+
+
+--
 -- Verify that the function accepts a string as its argument:
+--
 function TestAssertionPass.testPositive01()
     pass("The pass() function expects a string as its argument.")
 end
 
+
+
+--
+-- Verify that the function accepts an empty string as its argument:
+--
+function TestAssertionPass.testPositive02()
+    pass("")
+end
+
+
+
+--
 -- Verify that the function requires at least one argument:
+--
 function TestAssertionPass.testNegative01()
     pass()
 end
 
+
+
+--
 -- Verify that the function does not accept nil as an argument:
+--
 function TestAssertionPass.testNegative02()
     pass(nil)
 end
 
+
+
+--
 -- Verify that the function does not accept a boolean as an argument:
+--
 function TestAssertionPass.testNegative03()
     pass(true)
 end
 
+
+
+--
 -- Verify that the function does not accept a boolean as an argument:
 -- (and "false" sometimes has the same meaning as "nil", so check it)
+--
 function TestAssertionPass.testNegative04()
     pass(false)
 end
 
+
+
+--
 -- Verify that the function does not accept an integer as an argument:
+--
 function TestAssertionPass.testNegative05()
     pass(42)
 end
 
+
+
+--
 -- Verify that the function does not accept a real number as an argument:
+--
 function TestAssertionPass.testNegative06()
     pass(42.1)
 end
 
--- Verify that the function does not accept a table as an argument:
+
+
+--
+-- Verify that the function does not accept an empty table as an argument:
+--
 function TestAssertionPass.testNegative07()
+    pass({})
+end
+
+
+
+--
+-- Verify that the function does not accept a table as an argument:
+--
+function TestAssertionPass.testNegative08()
     pass({1,2,3})
 end
 
+
+
+--
+-- Verify that the function does not accept a hash table as an argument:
+--
+function TestAssertionPass.testNegative09()
+    pass({{{1},2},3})
+end
+
+
+
+--
+-- Verify that the function does not accept a nested table as an argument:
+--
+function TestAssertionPass.testNegative10()
+    local table1 = {}
+
+    -- fill in the first table
+    table1["first"]  = 1
+    table1["second"] = 2
+    table1["third"]  = 3
+
+    pass(table1)
+end
+
+
+
+--
 -- Verify that the function does not accept a standard function as an argument:
-function TestAssertionPass.testNegative08()
+--
+function TestAssertionPass.testNegative11()
     pass(print)
 end
 
+
+
+--
 -- Verify that the function does not accept an user defined function as an argument:
-function TestAssertionPass.testNegative09()
+--
+function TestAssertionPass.testNegative12()
+    -- create local function
     local func = function()
         return 42
     end
+    -- and call the pass() function with the function as its argument
     pass(func)
 end
 
+
+
+--
 -- Verify that the function does not accept a closure as an argument:
-function TestAssertionPass.testNegative10()
+--
+function TestAssertionPass.testNegative13()
+    -- create local closure
     local closure = function()
         local i
         return function()
@@ -88,14 +178,21 @@ function TestAssertionPass.testNegative10()
             return i
         end
     end
+    -- and call the pass() function with the closure as its argument
     pass(closure)
 end
 
+
+
+--
 -- Verify that the function does not accept a coroutine as an argument:
-function TestAssertionPass.testNegative11()
+--
+function TestAssertionPass.testNegative14()
+    -- create local coroutine
     local func = coroutine.create(function ()
            print(42)
          end)
+    -- and call the pass() function with the coroutine as its argument
     pass(func)
 end
 
