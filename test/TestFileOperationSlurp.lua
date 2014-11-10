7dbdfff28bd920aa3edc75a421a599a7f0fed22d
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-11-10 15:22:51 +0100

    Updated test TestFileOperationSlurp.lua.

--- test/TestFileOperationSlurp.lua
+++ test/TestFileOperationSlurp.lua
@@ -1,4 +1,6 @@
--- TestFileOperationSlurp.lua - check file I/O operations.
+-- TestFileOperationSlurp.lua - check file I/O operations:
+--     the function slurp().
+--
 -- Copyright (C) 2014 Pavel Tisnovsky
 --
 -- This file is part of Emender.
@@ -24,16 +26,15 @@ TestFileOperationSlurp = {
         emails = "ptisnovs@redhat.com",
         changed = "2014-11-07",
         tags = {"BasicTest", "FileIO"},
-    },
-    -- any other values are arbitrary
-    val = 42}
+    }
+}
 
 
 
 --
 -- Positive test: check if it's possible to read from /etc/passwd
 --
-function TestFileOperationSlurp.testSlurp1()
+function TestFileOperationSlurp.testSlurp01()
     -- try to read file
     local content = slurp("/etc/passwd")
     -- check if file could be read
@@ -43,9 +44,21 @@ end
 
 
 --
+-- Positive test: check if it's possible to read from /etc/group
+--
+function TestFileOperationSlurp.testSlurp02()
+    -- try to read file
+    local content = slurp("/etc/group")
+    -- check if file could be read
+    is_not_nil(content, "/etc/group file should be readable")
+end
+
+
+
+--
 -- Positive test: check if it's possible to read from /etc/fstab
 --
-function TestFileOperationSlurp.testSlurp2()
+function TestFileOperationSlurp.testSlurp03()
     -- try to read file
     local content = slurp("/etc/fstab")
     -- check if file could be read
@@ -55,9 +68,21 @@ end
 
 
 --
+-- Positive test: check if it's possible to read from /etc/issue
+--
+function TestFileOperationSlurp.testSlurp04()
+    -- try to read file
+    local content = slurp("/etc/issue")
+    -- check if file could be read
+    is_not_nil(content, "/etc/issue file should be readable")
+end
+
+
+
+--
 -- Negative test: check if it is not possible to read from /foo/bar/baz
 --
-function TestFileOperationSlurp.testSlurp3()
+function TestFileOperationSlurp.testSlurp05()
     -- try to read file
     local content = slurp("/foo/bar/baz")
     -- check if file can't be read
@@ -69,7 +94,7 @@ end
 --
 -- Negative test: check if it is not possible to read from ""
 --
-function TestFileOperationSlurp.testSlurp4()
+function TestFileOperationSlurp.testSlurp06()
     -- try to read file
     local content = slurp("")
     -- check if file can't be read
@@ -81,7 +106,7 @@ end
 --
 -- Negative test: check if it is not possible to read from "?"
 --
-function TestFileOperationSlurp.testSlurp5()
+function TestFileOperationSlurp.testSlurp07()
     -- try to read file
     local content = slurp("?")
     -- check if file can't be read
@@ -93,10 +118,34 @@ end
 --
 -- Negative test: check if it is not possible to read from "*"
 --
-function TestFileOperationSlurp.testSlurp6()
+function TestFileOperationSlurp.testSlurp08()
     -- try to read file
     local content = slurp("*")
     -- check if file can't be read
     is_nil(content, "* file can't be readable")
+end
+
+
+
+--
+-- Negative test: check if it is not possible to read from "."
+--
+function TestFileOperationSlurp.testSlurp09()
+    -- try to read file
+    local content = slurp(".")
+    -- check if file can't be read
+    is_nil(content, ". file can't be readable")
+end
+
+
+
+--
+-- Negative test: check if it is not possible to read from "/proc/kcore"
+--
+function TestFileOperationSlurp.testSlurp10()
+    -- try to read file
+    local content = slurp("/proc/kcore")
+    -- check if file can't be read
+    is_nil(content, "/proc/kcore file can't be readable")
 end
 
