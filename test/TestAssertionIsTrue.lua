0856006f10217ac6e22d11ac943beb117dee77a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-11-18 11:30:54 +0100

    Added new test cases.

--- test/TestAssertionIsTrue.lua
+++ test/TestAssertionIsTrue.lua
@@ -136,7 +136,7 @@ end
 
 
 --
--- Verify that the function does not accept an nil as an first argument:
+-- Verify that the function does not accept an nil as the first argument:
 --
 function TestAssertionIsTrue.testNegative07()
     is_true(nil, "What's wrong?")
@@ -154,7 +154,7 @@ end
 
 
 --
--- Verify that the first argument has type boolean:
+-- Verify that the function does not accept an integer as the first argument:
 --
 function TestAssertionIsTrue.testNegative09()
     is_true(42, "42 is not a valid expression")
@@ -163,20 +163,232 @@ end
 
 
 --
--- Verify that the second argument has type string:
+-- Verify that the function does not accept a real number as the first argument:
 --
 function TestAssertionIsTrue.testNegative10()
-    is_true(1==1, 42)
+    is_true(42.1, "42.1 is not a valid expression")
 end
 
 
 
 --
--- Verify that the second argument has type string:
+-- Verify that the function does not accept an empty table as the first argument:
 --
 function TestAssertionIsTrue.testNegative11()
-    is_true(1==1, nil)
+    is_true({}, "{} is not a valid expression")
 end
 
 
+
+--
+-- Verify that the function does not accept a table as the first argument:
+--
+function TestAssertionIsTrue.testNegative12()
+    is_true({1,2,3}, "{1,2,3} is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept nested table as the first argument:
+--
+function TestAssertionIsTrue.testNegative13()
+    is_true({{{1},2},3}, "{{{1},2},3} is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept a hash table as an argument:
+--
+function TestAssertionIsTrue.testNegative14()
+    local table1 = {}
+
+    -- fill in the first table
+    table1["first"]  = 1
+    table1["second"] = 2
+    table1["third"]  = 3
+
+    is_true(table1, "hash table is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept a standard function as an argument:
+--
+function TestAssertionIsTrue.testNegative15()
+    is_true(print, "function is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept an user defined function as an argument:
+--
+function TestAssertionIsTrue.testNegative16()
+    -- create local function
+    local func = function()
+        return 42
+    end
+    -- and call the is_true() function with the function as its argument
+    is_true(func, "function is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept a closure as an argument:
+--
+function TestAssertionIsTrue.testNegative17()
+    -- create local closure
+    local closure = function()
+        local i
+        return function()
+            i = i + 1
+            return i
+        end
+    end
+    -- and call the is_true() function with the closure as its argument
+    is_true(closure, "closure is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept a coroutine as an argument:
+--
+function TestAssertionIsTrue.testNegative18()
+    -- create local coroutine
+    local func = coroutine.create(function ()
+           print(42)
+         end)
+    -- and call the is_true() function with the coroutine as its argument
+    is_true(func, "coroutine is not a valid expression")
+end
+
+
+
+--
+-- Verify that the function does not accept an nil as a second argument:
+--
+function TestAssertionIsTrue.testNegative19()
+    is_true(true, nil)
+end
+
+
+
+--
+-- Verify that the function does not accept an integer as a second argument:
+--
+function TestAssertionIsTrue.testNegative20()
+    is_true(true, 42)
+end
+
+
+
+--
+-- Verify that the function does not accept real number as a second argument:
+--
+function TestAssertionIsTrue.testNegative21()
+    is_true(true, 42.1)
+end
+
+
+
+--
+-- Verify that the function does not accept an empty table as second argument:
+--
+function TestAssertionIsTrue.testNegative22()
+    is_true(true, {})
+end
+
+
+
+--
+-- Verify that the function does not accept a table as second argument:
+--
+function TestAssertionIsTrue.testNegative23()
+    is_true(true, {1,2,3})
+end
+
+
+
+--
+-- Verify that the function does not accept nested table as second argument:
+--
+function TestAssertionIsTrue.testNegative24()
+    is_true(true, {{{1},2},3})
+end
+
+
+
+--
+-- Verify that the function does not accept a hash table as an argument:
+--
+function TestAssertionIsTrue.testNegative25()
+    local table1 = {}
+
+    -- fill in the first table
+    table1["first"]  = 1
+    table1["second"] = 2
+    table1["third"]  = 3
+
+    is_true(true, table1)
+end
+
+
+
+--
+-- Verify that the function does not accept a standard function as an argument:
+--
+function TestAssertionIsTrue.testNegative26()
+    is_true(true, print)
+end
+
+
+
+--
+-- Verify that the function does not accept an user defined function as an argument:
+--
+function TestAssertionIsTrue.testNegative27()
+    -- create local function
+    local func = function()
+        return 42
+    end
+    -- and call the is_true() function with the function as its argument
+    is_true(true, func)
+end
+
+
+
+--
+-- Verify that the function does not accept a closure as an argument:
+--
+function TestAssertionIsTrue.testNegative28()
+    -- create local closure
+    local closure = function()
+        local i
+        return function()
+            i = i + 1
+            return i
+        end
+    end
+    -- and call the is_true() function with the closure as its argument
+    is_true(true, closure)
+end
+
+
+
+--
+-- Verify that the function does not accept a coroutine as an argument:
+--
+function TestAssertionIsTrue.testNegative29()
+    -- create local coroutine
+    local func = coroutine.create(function ()
+           print(42)
+         end)
+    -- and call the is_true() function with the coroutine as its argument
+    is_true(true, func)
+end
 
