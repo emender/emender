fc5a810c2c7fd67de10235ab9a64fe49439e9983
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-11-24 14:02:00 +0100

    Added example how to pass parameters to the test.

---
+++ test/TestParameters.lua
@@ -0,0 +1,43 @@
+-- TestParameters.lua - check that parameters could be passed to test
+-- Copyright (C) 2014 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestParameters = {
+    -- required field
+    metadata = {
+        description = "Check that parameters could be passed to test",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-11-24",
+        tags = {"BasicTest", "SmokeTest"},
+    },
+}
+
+function TestParameters.testParam1()
+    is_not_nil(TestParameters.param1, "Check that param1 is not nil") 
+    if TestParameters.param1 then
+        is_equal(TestParameters.param1, "42", "Check that param1=='42'") 
+    end
+end
+
+function TestParameters.testParam2()
+    is_not_nil(TestParameters.param2, "Check that param2 is not nil") 
+    if TestParameters.param2 then
+        is_equal(TestParameters.param2, "unknown", "Check that param1=='unknown'") 
+    end
+end
+
