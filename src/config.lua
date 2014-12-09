113b671f95cea74a86aaddc60c3131fb53f489dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-12-09 14:02:17 +0100

    Added support for loading global configuration file.

---
+++ src/config.lua
@@ -0,0 +1,24 @@
+-- Global Emender configuration.
+-- Copyright (C) 2014 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+
+
+-- If set to true, graph with overall results will be generated
+-- in HTML output. This graph would require flotr library.
+generateGraphsInHtmlOutput = false
+
