33c8d22776e98521eb0144b8d98b455538a2f64d
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-12-12 13:18:28 +0100

    Added new test TestSetUpOnlyFailure.

---
+++ test/TestSetUpOnlyFailure.lua
@@ -0,0 +1,37 @@
+-- TestSetUpOnlyFailure.lua - check for basic Emender functionality
+-- Copyright (C) 2014 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestSetUpOnlyFailure = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2014-12-12",
+        tags = {"BasicTest"},
+    }
+}
+
+--
+-- setUp() function fails that means that the whole test should
+-- fail immediatelly.
+--
+function TestSetUpOnlyFailure.setUp()
+    fail("setUp() failure.")
+end
+
