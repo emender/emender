034d571fbaca5a490012567ff01242b1c52216cf
Pavel Tisnovsky <ptisnovs@localhost.localdomain>
2015-02-17 10:06:56 +0100

    Added three new tests: TestTagATagB, TestTagATagC and TestTagBTagC.

---
+++ test/TestTagBTagC.lua
@@ -0,0 +1,37 @@
+-- TestTagBTagC.lua - check for basic Emender functionality: support for tags.
+--
+-- Copyright (C) 2015 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestTagBTagC = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2015-02-12",
+        tags = {"tagB", "tagC"}
+    }
+}
+
+--
+-- Comment bound to the function TestTagBTagC.test1().
+--
+function TestTagBTagC.test1()
+    pass("The function TestTagBTagC.test1() is called properly.")
+end
+
