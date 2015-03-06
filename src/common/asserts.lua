4b78bb0d2cbeae901024ae7be8fad955a7ac58bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-03-06 13:53:20 +0100

    Minor improvement.

--- src/common/asserts.lua
+++ src/common/asserts.lua
@@ -1,8 +1,8 @@
 -- asserts.lua - a module containing all required assertion functions.
 -- Copyright (C) 2014, 2015  Pavel Tisnovsky, Jaromir Hradilek
-
+--
 -- This file is part of Emender.
-
+--
 -- Emender is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation; version 3 of the License.
