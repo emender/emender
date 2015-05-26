088cc14676ccca970f075c70150fc7cc3d80a875
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-05-26 13:25:53 +0200

    Added test that checks "requires" attribute.

---
+++ test/TestRequires3.lua
@@ -0,0 +1,36 @@
+-- TestRequires3.lua - check if all required tools are available.
+--
+-- Copyright (C) 2015 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestRequires3 = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2015-05-26",
+        tags = {"BasicTest", "SmokeTest"},
+    },
+    -- check if the tool work properly with empty table
+    requires = {"unknown-utility"}
+}
+
+function TestRequires3.testA()
+    print("\tTestRequires2.testA()")
+end
+
