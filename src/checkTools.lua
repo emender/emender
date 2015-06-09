0a46f9cbabe1fe7889d827a1859dc2a261f7ba7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-06-09 11:26:55 +0200

    Stub for new module - checkTools.

---
+++ src/checkTools.lua
@@ -0,0 +1,31 @@
+-- Module that checks if all tools needed to run selected tests are installed.
+-- Copyright (C) 2015 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+
+
+--
+-- Module name
+--
+local checkTools = {
+}
+
+
+
+-- export module 
+return checkTools
+
