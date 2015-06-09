cd126b6cc5c725327841e76ca6918b8841eff8d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-06-09 11:23:12 +0200

    Added new selfcheck for TestRequires7.lua test.

--- selfchecks/selfcheck.sh
+++ selfchecks/selfcheck.sh
@@ -156,6 +156,7 @@ function runOtherTests {
      ../emend ../test/TestRequires4.lua        > test_requires4.out
      ../emend ../test/TestRequires5.lua        > test_requires5.out
      ../emend ../test/TestRequires6.lua        > test_requires6.out
+     ../emend ../test/TestRequires7.lua        > test_requires7.out
      ../emend ../test/TestExec.lua             > test_exec.out
 }
 
