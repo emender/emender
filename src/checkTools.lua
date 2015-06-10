383ff968766e549c34a0b8cc85657c56f8384e64
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-06-10 13:25:37 +0200

    Refactored checkTools function into its own module.

--- src/checkTools.lua
+++ src/checkTools.lua
@@ -26,6 +26,32 @@ local checkTools = {
 
 
 
+function checkForAllTools(testSuite, testSuiteName)
+    local test = _G[testSuiteName]
+    if not test then
+        return false
+    end
+    local requires = test["requires"]
+
+    -- no external tools needed -> everything is ok in this step
+    -- (note that "not nil" condition can't be used here because we need to
+    --  distinguish between "false" and "nil)
+    if requires == nil then
+        return true
+    else
+        -- make sure the test structure is correct, ie if the 'requires'
+        -- metadata is table (and not other type)
+        if type(requires) == "table" then
+            return toolsChecker.performCheck(true, requires, false)
+        else
+            print("Test structure error: wrong 'requires' attribute - table expected")
+            return false
+        end
+    end
+end
+
+
+
 -- export module 
 return checkTools
 
--- src/core.lua
+++ src/core.lua
@@ -234,32 +234,6 @@ end
 
 
 
-function checkTools(testSuite, testSuiteName)
-    local test = _G[testSuiteName]
-    if not test then
-        return false
-    end
-    local requires = test["requires"]
-
-    -- no external tools needed -> everything is ok in this step
-    -- (note that "not nil" condition can't be used here because we need to
-    --  distinguish between "false" and "nil)
-    if requires == nil then
-        return true
-    else
-        -- make sure the test structure is correct, ie if the 'requires'
-        -- metadata is table (and not other type)
-        if type(requires) == "table" then
-            return toolsChecker.performCheck(true, requires, false)
-        else
-            print("Test structure error: wrong 'requires' attribute - table expected")
-            return false
-        end
-    end
-end
-
-
-
 --
 -- Fill in all required information in the testSuite record from the metadata
 -- read from the test script.
@@ -455,7 +429,7 @@ function core.runTest(scriptDirectory, filename, verboseOperation, testOptions, 
 
         local processRestOfTest = true
 
-        if not checkTools(testSuite, testSuiteName) then
+        if not checkForAllTools(testSuite, testSuiteName) then
             print("Required tool(s) missing.")
             processRestOfTest = false
         end
