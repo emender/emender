187cc68f53c319c147edf62f29b183c7c4602046
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-08-20 18:03:54 +0200

    Two new tests: TestRequiresB and TestRequiresC.

---
+++ test/TestRequiresB.lua
@@ -0,0 +1,53 @@
+-- TestRequiresB.lua - check if all required tools are available.
+-- This test contains 'requires' metadata with one non-existing tool.
+--
+-- Copyright (C) 2015 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestRequiresB = {
+    -- required field
+    metadata = {
+        description = "Check that core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2015-05-27",
+        tags = {"BasicTest", "SmokeTest"},
+    },
+    -- check if the Emender core properly checks for existence of
+    -- unknown utility (-> this test should fail)
+    requires = {"wget", "ls", "unknown-utility"}
+}
+
+
+
+--
+-- Set up function.
+--
+function TestRequiresB.setUp()
+    pass("TestRequiresB.setUp()")
+end
+
+
+
+--
+-- Just a dummy test function that shoult always pass. This function is
+-- included here because we would like to have non-empty test.
+--
+function TestRequiresB.testA()
+    pass("TestRequiresB.testA()")
+end
+
---
+++ test/TestRequiresC.lua
@@ -0,0 +1,53 @@
+-- TestRequiresC.lua - check if all required tools are available.
+-- This test contains 'requires' metadata which has wrong type.
+--
+-- Copyright (C) 2014, 2015 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+TestRequiresC = {
+    -- required field
+    metadata = {
+        description = "Check that docunit core works correctly.",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2015-05-27",
+        tags = {"BasicTest", "SmokeTest", "RequireMetadataTest"},
+    },
+    -- check if the Emender core work properly with 'requires' attribute
+    -- that has wrong type
+    requires = true
+}
+
+
+
+--
+-- Set up function.
+--
+function TestRequiresC.setUp()
+    pass("TestRequiresC.setUp()")
+end
+
+
+
+--
+-- Just a dummy test function that shoult always pass. This function is
+-- included here because we would like to have non-empty test.
+--
+function TestRequiresC.testA()
+    pass("TestRequiresC.testA()")
+end
+
