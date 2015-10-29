1647940818879c22a07431501adff30fc47aa454
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-29 10:58:44 +0100

    Added preambule into commonTextWriter module.

--- src/writers/commonTextWriter.lua
+++ src/writers/commonTextWriter.lua
@@ -1,3 +1,22 @@
+-- colorTextWriter.lua - support for terminal output (using colors if possible).
+-- Copyright (C) 2015 Jaromir Hradilek, Pavel Tisnovsky
+
+-- This file is part of Emender.
+
+-- This program is free software:  you can redistribute it and/or modify it
+-- under the terms of  the  GNU General Public License  as published by the
+-- Free Software Foundation, version 3 of the License.
+--
+-- This program  is  distributed  in the hope  that it will be useful,  but
+-- WITHOUT  ANY WARRANTY;  without  even the implied warranty of MERCHANTA-
+-- BILITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+-- License for more details.
+--
+-- You should have received a copy of the GNU General Public License  along
+-- with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
 local textWidth = 75
 local horizontalSeparator = string.rep("-", 75) .. "\n"
 
