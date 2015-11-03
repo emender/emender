b3f6c1e0d1b11738f2e124a9ddf43e86f30db1ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-03 14:04:01 +0100

    Two new tests: TestModuleString.testAlignRight12 and TestModuleString.testAlignRight13

--- test/TestModuleString.lua
+++ test/TestModuleString.lua
@@ -1633,6 +1633,31 @@ end
 
 
 
+--
+-- Test the behaviour of the following Emender function: string:alignRight().
+--
+function TestModuleString.testAlignRight12()
+    is_equal("                    word", string.alignRight("word", 10, 20),       "Test if alignRight() function works correctly")
+    is_equal("                    word ", string.alignRight("word ", 10, 20),     "Test if alignRight() function works correctly")
+    is_equal("                    word  ", string.alignRight("word  ", 10, 20),   "Test if alignRight() function works correctly")
+    is_equal("                    word   ", string.alignRight("word   ", 10, 20), "Test if alignRight() function works correctly")
+end
+
+
+
+--
+-- Test the behaviour of the following Emender function: string:alignRight().
+--
+function TestModuleString.testAlignRight13()
+    is_equal("                    word", string.alignRight("word", 20, 20),       "Test if alignRight() function works correctly")
+    is_equal("                    word ", string.alignRight("word ", 20, 20),     "Test if alignRight() function works correctly")
+    is_equal("                    word  ", string.alignRight("word  ", 20, 20),   "Test if alignRight() function works correctly")
+    is_equal("                    word   ", string.alignRight("word   ", 20, 20), "Test if alignRight() function works correctly")
+end
+
+
+
+
 -- TODO:
 -- string.alignRight(str, width, indent, first_indent)
 -- string.alignCenter(str, width, indent, first_indent)
