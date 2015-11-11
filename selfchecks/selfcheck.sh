0a442eb3693ed1c2adb407077357c514885a6264
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-11 11:16:23 +0100

    Two new selfchecks: bad_output_format_6 and bad_output_format_7

--- selfchecks/selfcheck.sh
+++ selfchecks/selfcheck.sh
@@ -148,11 +148,13 @@ function runTestsWithUnknownCLIOptions {
 
 # Run Emender by using various bad output format
 function runTestsWithBadOutputFormat {
-    ../emend -o output1 ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_1.out
-    ../emend -o out.    ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_2.out
-    ../emend -o out..   ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_3.out
-    ../emend -o out.doc ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_4.out
-    ../emend -o out.X   ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_5.out
+    ../emend -o output1  ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_1.out
+    ../emend -o out.     ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_2.out
+    ../emend -o out..    ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_3.out
+    ../emend -o out.doc  ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_4.out
+    ../emend -o out.X    ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_5.out
+    ../emend -o out.htm  ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_6.out
+    ../emend -o out.xhtm ../test/TestPassFail.lua ../test/Test1.lua > bad_output_format_7.out
 }
 
 
