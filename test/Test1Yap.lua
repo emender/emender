8c29f9ea8d6762efb6130d021953370596530b4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-12-08 13:01:29 +0100

    New test for checking the yap() function

---
+++ test/Test1Yap.lua
@@ -0,0 +1,39 @@
+-- Test1Yap.lua - check the graph & statistic submodules of Emender.
+-- Copyright (C) 2014 Pavel Tisnovsky
+--
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+Test1Yap = {
+    -- required field
+    metadata = {
+        description = "Check the graph & statistic submodules of Emender",
+        authors = "Pavel Tisnovsky",
+        emails = "ptisnovs@redhat.com",
+        changed = "2015-12-08",
+        tags = {"BasicTest", "SmokeTest"},
+    },
+}
+
+
+
+--
+-- This function calls yap() once.
+--
+function Test1Yap.test()
+    yap("Yap#1")
+end
+
