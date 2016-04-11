dc70355787d5ff91fa6aba12d69ddd0d4a4e36a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-11 11:49:18 +0200

    New module with stub for REST API service

---
+++ src/service.lua
@@ -0,0 +1,47 @@
+-- Module with the implementation of REST API service interface.
+-- Copyright (C) 2016 Pavel Tisnovsky
+--
+-- This file is part of Emender.
+--
+-- Emender is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 3 of the License.
+--
+-- Emender is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Emender.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+
+
+--
+-- Module name
+--
+local service = {
+}
+
+
+
+--
+-- Function to call REST API service when job has been started.
+--
+function service.jobStarted()
+end
+
+
+
+--
+-- Function to call REST API service when job has been finished.
+--
+function service.jobStarted()
+end
+
+
+
+-- export module
+return service
+
