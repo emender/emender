a8b04819eddc7d7815c243cda7bae0faa6990393
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-10-10 16:25:43 +0200

    Added licence text into AsciiDoc2Docbook45.sh and AsciiDoc2Docbook50.sh scripts

--- test_examples/AsciiDoc2Docbook45.sh
+++ test_examples/AsciiDoc2Docbook45.sh
@@ -2,6 +2,20 @@
 
 # Example script that converts books from AsciiDoc format to the DocBook 4.5.
 
+# Copyright (C) 2016 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+# This program is  free software:  you can redistribute it and/or modify it
+# under  the terms  of the  GNU General Public License  as published by the
+# Free Software Foundation, version 3 of the License.
+#
+# This program  is  distributed  in the hope  that it will  be useful,  but
+# WITHOUT  ANY WARRANTY;  without  even the implied  warranty of MERCHANTA-
+# BILITY  or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the  GNU General Public License  along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 MASTER=master
 rm -rf ${MASTER}
 
