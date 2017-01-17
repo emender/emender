395d889aed941bc08e9c8f9d4f21b533c5e9b5bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-17 15:55:47 +0100

    Fourth test for iniFileReader

--- test/TestIniFileReader.lua
+++ test/TestIniFileReader.lua
@@ -51,6 +51,25 @@ function TestIniFileReader.testIniFile2()
     end
 end
 
+function TestIniFileReader.testIniFile3()
+    local data = iniFileReader.readIni("test/ini3.ini")
+
+    is_not_nil(data.section1, "Check if returned table contains expected section [section1]")
+    is_not_nil(data.section2, "Check if returned table contains expected section [section2]")
+    if data.section1 and data.section2 then
+        is_not_nil(data.section1.name1, "Check the line name1=value1")
+        is_not_nil(data.section1.name2, "Check the line name2=value2")
+        is_equal(data.section1.name1, "value1", "Check the line name1=value1")
+        is_equal(data.section1.name2, "value2", "Check the line name2=value2")
+        is_not_nil(data.section2.name1, "Check the line name1=value1")
+        is_not_nil(data.section2.name2, "Check the line name2=value2")
+        is_equal(data.section2.name1, "value1", "Check the line name1=value1")
+        is_equal(data.section2.name2, "value2", "Check the line name2=value2")
+    else
+        fail("INI was not read properly")
+    end
+end
+
 function TestIniFileReader.testMissingIniFile()
     local data = iniFileReader.readIni("test/missing.ini")
     is_empty(data, "Read structure must be empty")
