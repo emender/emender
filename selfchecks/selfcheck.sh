063adec32a2281dc1fec061d4ce5a1ba3b08b603
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-17 17:50:43 +0100

    Test results should be independent of directory structure

--- selfchecks/selfcheck.sh
+++ selfchecks/selfcheck.sh
@@ -331,7 +331,7 @@ function runOtherTests {
      ../emend --Xparam1=42  --Xparam2=42          ../test/TestParameters.lua > test_parameters_7.out
      ../emend --Xparam1=42  --Xparam2=unknown     ../test/TestParameters.lua > test_parameters_8.out
      pushd ini
-     ../../emend --Xparam1=42  --Xparam2=x        ../../test/TestParameters.lua > ../test_parameters_9.out
+     ../../emend --Xparam1=42  --Xparam2=x        ../../test/TestParameters.lua | tail -n +2 > ../test_parameters_9.out
      popd
 }
 
